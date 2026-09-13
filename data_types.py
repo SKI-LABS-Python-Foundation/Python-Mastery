3af5e6af5fb9b2c147733c78dc1dbbe41e0d80a6
SKI-LABS-Python-Foundation <denismourice6@gmail.com>
2026-09-13 21:34:16 +0300

    First commit

---
+++ data_types.py
@@ -0,0 +1,22 @@
+# data - 23, 45, 67, 88, Joseph
+# integers - whole numbers
+# float - decimal numbers
+# string - letter/characters
+# boolean - false/true
+
+
+username = ''
+weight = 67.89
+age = 45
+isActive = True
+
+
+print(type(username))
+print(type(weight))
+print(type(age))
+print(type(isActive))
+
+
+"""
+A school wants a simple Python program that can help a teacher calculate a student's academic performance. The program should ask the teacher to enter the student's name, Mathematics marks, Programming marks, and Networking marks. After receiving the information, the program should calculate the student's total marks and average mark and display a clear summary showing the student's name, total marks, and average.
+"""
---
+++ hello.py
@@ -0,0 +1,15 @@
+# COMMENTS - statements that are to document a code/ explaining - single line
+'''
+multiline comments 
+'''
+
+# VARIABLES - container stores data in program/ named memory location
+
+username = "SKILABS"
+myage = 45
+name_of_school = "MIT" 
+_gender = "male"
+
+# functions
+print(username)
+print("Welcome to python course")
